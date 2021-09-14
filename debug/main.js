2c2eab02059190511186197be6595634c6c24a0f
NoobMx <roberto_1320114135@uptecamac.edu.mx>
2021-09-14 13:06:26 -0500

    Practica13092021

---
+++ debug/main.js
@@ -0,0 +1,56 @@
+// var n1 = 30;
+// var n2 = 20;
+// var test = 0;
+// if(n1 < n2) {
+//     console.log("n1 es menor n2");
+// }else{
+//     console.log("n1 mayor n2");
+//     test = suma();
+// }
+// n1 = 100;
+
+// console.log(n1);
+// console.log(test);
+
+// function suma(){    
+//     let resultado = n1 + n2;
+//     return resultado;
+// }
+
+// var elementos =[];
+// var elementos2=Array();
+// var elementos3=["hola", 1, 8.5, true, 5, 6, 7, 8, 9, 10];
+// var elementos4=[];
+
+// for(let i=0; i<elementos3.length; i++){
+//     console.log(elementos3[i]);
+//     elementos4.push(elementos3[i]);
+// }
+// console.log(elementos4);   
+
+//var contenedor=[];
+//var palabra="Hola mundo";
+
+
+//for (let i = 0; i < palabra.length; i++) {
+    //contenedor.push(palabra[i]);
+//}
+//console.log(contenedor);
+
+var miNombre={
+    "Nombre":"Roberto Alexis",
+    "Apellidos":"Mejia Osorio",
+    "Edad":19,
+    "Datos escolares":{
+        "Escuela":"UPT(Universidad politecnica de Tecámac)",
+        "Grupo":"3421IS",
+        "Carrera":"Ingenieria en Software",
+        "Gustos":{
+            "Géneros musicales":"Rap, Reggae,Rock,Electronica...",
+            "Hobbis":"Videojuegos, montar bici, viajar, correr...",
+            "Lenguajes":"C#,C++,HTML...",
+        }
+    }
+}
+console.log(miNombre);
+
