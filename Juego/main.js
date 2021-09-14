654bb1de56b0d619345e96112e6252830ba908dd
NoobMx <roberto_1320114135@uptecamac.edu.mx>
2021-09-14 15:08:06 -0500

    practica14

---
+++ Juego/main.js
@@ -0,0 +1,8 @@
+var palabraSecreta="Chale";
+
+var palabra=prompt("Ingrese la palabra");
+if (palabra==palabraSecreta) {
+    console.log("ES LA PALABRA CORRECTA :)");
+} else {
+    console.log("ESTAS EQUIVOCADO :(");
+}
